8b2903bb50ca1ea66aebd2385e163d5c986b5c15
Brian K Fung <64922294+ufbfung@users.noreply.github.com>
2023-12-07 20:10:58 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import streamlit as st
 import requests
 import os
 from openai import OpenAI
-import html
+from lxml import html
 
 def search_pubmed(query_terms, max_articles=20):
     # Step 1: Perform a search and get a list of PubMed IDs
